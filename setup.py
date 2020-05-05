0572d30436c1bbed5727b2c176c26925cd1a3189
mtaghiza <mtaghiza@jhu.edu>
2020-05-05 18:46:58 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='SpectrumViewer',
+setup(name='specviewer',
       version='0.3',
       description='Spectrum Viewer',
       url='http://github.com/Subaru-PFS/pfs_specViewer',
       author='Manuchehr Taghizadeh-Popp, Zuo "David" Wang',
       author_email='mtaghiza@jhu.edu',
       license='GNU',
-      packages=['SpectrumViewer'],
+      packages=['specviewer'],
       zip_safe=False,
       include_package_data=True
       )
